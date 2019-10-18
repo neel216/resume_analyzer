7ff8b5c9f5d744201f5bb8f979ba060a5d1db47f
neel216 <neelr216@gmail.com>
2019-10-18 00:59:23 -0400

    added page count function and text finder

--- parser.py
+++ parser.py
@@ -2,35 +2,53 @@
 '''
 
 from docx import Document
+import win32com.client as win32
+import os
 
 
 class Resume:
 
     def __init__(self, path):
-        document = Document(path)
+        self.PATH = path # set resume path
+        
+        self.document = Document(self.PATH) # load word document
 
-        paragraph_list = document.paragraphs
+        paragraph_list = self.document.paragraphs # find paragraphs in text
         self.content = []
 
+        # add each paragraph to content list
         for paragraph in paragraph_list:
             self.content.append(paragraph.text)
 
-    def print_content(self):
-        print(self.content)
+    def get_content(self):
+        return self.content
     
+    def get_page_count(self):
+        try:
+            # open word document
+            word = win32.gencache.EnsureDispatch('Word.Application')
+            word.Visible = False
+            doc_path = os.getcwd() + self.PATH
+            doc = word.Documents.Open(doc_path)
+            
+            # get number of sheets
+            doc.Repaginate()
+            self.pages = doc.ComputeStatistics(2)
 
+            # close word
+            doc.Close()
+            word.Quit()
 
+            return self.pages
+        
+        # if error while loading document or getting page count
+        except:
+            print('[ERROR] Error Checking Page Count. Make sure document is not in Protected View Mode.')
+        
+        return None
 
-#document = Document('./test_data/resume_template.docx')
-
-#paragraph_list = document.paragraphs
-#content = []
-
-#for paragraph in paragraph_list:
-#    content.append(paragraph.text)
-
-#print(content)
-
+    
 if __name__ == "__main__":
     resume = Resume('./test_data/resume_template.docx')
-    resume.print_content()
+    # resume.print_content()
+    print(f'Detected {resume.get_page_count()} pages in resume')
