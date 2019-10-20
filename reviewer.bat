5af9d60f8c14d8a01f7190941d33abfd924f295f
neel216 <neelr216@gmail.com>
2019-10-20 17:55:19 -0400

    .exe to pip

--- reviewer.bat
+++ reviewer.bat
@@ -1,5 +1,5 @@
 for /d %%d in (C:\Users\%USERNAME%\AppData\Local\Programs\Python\*) do (set profile=%%d& goto break)
 :break
-%profile%\Scripts pip install -r requirements.txt
+%profile%\Scripts\pip.exe install -r requirements.txt
 %profile%\python.exe setup.py
 %profile%\python.exe reviewer.py
